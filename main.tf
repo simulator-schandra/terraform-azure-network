4e50ba6459d9001493e510c54a696cd7adaf4fee
Suyash Chandra <suyash.chandra@fci-ccm.com>
2024-08-25 10:18:10 +0530

    suyash : vnet : done

--- main.tf
+++ main.tf
@@ -8,12 +8,13 @@ module "rg" {
 }
 
 module "vnet" {
-    source = "./module/virtual_network"
-    vnet_name = "simulator-vnet"
-    vnet_location = "South India"
-    rg_name = "simulator-rg"
-    vnet_address_space = ["10.0.0.0/16"]
-    tags = {
-      "Environment" = "Staging"
-    }
+  source = "./module/virtual_network"
+  vnet_name = "simulator-vnet"
+  vnet_location = "South India"
+  rg_name = "simulator-rg"
+  vnet_address_space = ["10.0.0.0/16"]
+  tags = {
+    "Environment" = "Staging"
+  }
+  
 }
--- module/virtual_network/variable.tf
+++ module/virtual_network/variable.tf
@@ -18,29 +18,29 @@ variable "tags" {
   type = map(string)
 }
 
-variable "vnet_dns_servers" {
-  type = list(string)
-  default = []
-}
+# variable "vnet_dns_servers" {
+#   type = list(string)
+#   default = []
+# }
 
-variable "vnet_subnet" {
-  type = list(object({
-    name                                          = string
-    address_prefixes                              = list(string)
-    security_group                                = string   # Optional: Referencing a security group
-    default_outbound_access_enabled               = bool     # Optional: Setting outbound access
-    delegation                                    = list(object({
-      name = string
-      service_delegation = list(object({
-        actions = list(string)
-        name    = string
-      }))
-    }))
-    private_endpoint_network_policies             = bool
-    private_link_service_network_policies_enabled = bool
-    route_table_id                                = string
-    service_endpoints                             = list(string)
-    service_endpoint_policy_ids                   = list(string)
-  }))
-  default = []
-}
+# variable "vnet_subnet" {
+#   type = list(object({
+#     name                                          = string
+#     address_prefixes                              = list(string)
+#     security_group                                = string   # Optional: Referencing a security group
+#     default_outbound_access_enabled               = bool     # Optional: Setting outbound access
+#     delegation                                    = list(object({
+#       name = string
+#       service_delegation = list(object({
+#         actions = list(string)
+#         name    = string
+#       }))
+#     }))
+#     private_endpoint_network_policies             = bool
+#     private_link_service_network_policies_enabled = bool
+#     route_table_id                                = string
+#     service_endpoints                             = list(string)
+#     service_endpoint_policy_ids                   = list(string)
+#   }))
+#   default = []
+# }
