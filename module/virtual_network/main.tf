ddaf7c976f17ad8221a77451135562be63e42761
Suyash Chandra <suyash.chandra@fci-ccm.com>
2024-08-25 10:08:08 +0530

    suyash : vnet : done

--- module/virtual_network/main.tf
+++ module/virtual_network/main.tf
@@ -4,7 +4,22 @@ resource "azurerm_virtual_network" "virtual_network" {
   resource_group_name = var.rg_name
   address_space       = var.vnet_address_space
   dns_servers         = var.vnet_dns_servers
-  subnet              = var.vnet_subnet
+
+  dynamic "subnet" {
+    for_each = var.vnet_subnet
+    content {
+      name                                          = subnet.value.name
+      address_prefixes                              = subnet.value.address_prefixes
+      security_group                                = subnet.value.security_group                  # If needed
+      default_outbound_access_enabled               = subnet.value.default_outbound_access_enabled # If needed
+      delegation                                    = subnet.value.delegation                      # If needed
+      private_endpoint_network_policies             = subnet.value.private_endpoint_network_policies
+      private_link_service_network_policies_enabled = subnet.value.private_link_service_network_policies_enabled
+      route_table_id                                = subnet.value.route_table_id
+      service_endpoints                             = subnet.value.service_endpoints
+      service_endpoint_policy_ids                   = subnet.value.service_endpoint_policy_ids
+    }
+  }
 
   tags = merge(
     {
