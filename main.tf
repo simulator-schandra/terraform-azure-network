029139b6eb0c5e6f694d704fabb148f7b30cd9e2
Suyash Chandra <suyash.chandra@fci-ccm.com>
2024-08-31 19:53:08 +0530

    suyash : rt : wip

--- main.tf
+++ main.tf
@@ -9,25 +9,6 @@ module "vnet" {
   }
 }
 
-module "public_subnet" {
-  source                          = "./module/subnet"
-  subnet_name                     = ["simulator-sub-pub-1", "simulator-sub-pub-2"]
-  rg_name                         = "simulator-rg"
-  vnet_name                       = "simulator-vnet"
-  subnet_cidr                     = [["10.0.0.0/23"], ["10.0.2.0/23"]]
-  default_outbound_access_enabled = true
-  depends_on                      = [module.vnet]
-}
-
-module "private_subnet" {
-  source      = "./module/subnet"
-  subnet_name = ["simulator-sub-pvt-1", "simulator-sub-pvt-2"]
-  rg_name     = "simulator-rg"
-  vnet_name   = "simulator-vnet"
-  subnet_cidr = [["10.0.4.0/23"], ["10.0.6.0/23"]]
-  depends_on  = [module.vnet]
-}
-
 module "nat" {
   source            = "./module/nat_gateway"
   nat_name          = "simulator-nat"
@@ -37,13 +18,7 @@ module "nat" {
   tags = {
     "Environment" = "Staging"
   }
-  depends_on = [module.private_subnet]
-}
-
-resource "azurerm_subnet_nat_gateway_association" "subnet_nat_gateway_association" {
-  count          = length([["10.0.4.0/23"], ["10.0.6.0/23"]])
-  subnet_id      = module.private_subnet.subnet_ids[count.index]
-  nat_gateway_id = module.nat.nat_id
+  depends_on = [module.vnet]
 }
 
 module "pvt_rt" {
@@ -66,7 +41,36 @@ module "pvt_rt" {
   tags = {
     "Environment" = "Staging"
   }
+  depends_on = [ module.vnet ]
 }
+
+module "public_subnet" {
+  source                          = "./module/subnet"
+  subnet_name                     = ["simulator-sub-pub-1", "simulator-sub-pub-2"]
+  rg_name                         = "simulator-rg"
+  vnet_name                       = "simulator-vnet"
+  subnet_cidr                     = [["10.0.0.0/23"], ["10.0.2.0/23"]]
+  default_outbound_access_enabled = true
+  depends_on                      = [module.nat]
+}
+
+module "private_subnet" {
+  source      = "./module/subnet"
+  subnet_name = ["simulator-sub-pvt-1", "simulator-sub-pvt-2"]
+  rg_name     = "simulator-rg"
+  vnet_name   = "simulator-vnet"
+  subnet_cidr = [["10.0.4.0/23"], ["10.0.6.0/23"]]
+  depends_on  = [module.nat]
+}
+
+resource "azurerm_subnet_nat_gateway_association" "subnet_nat_gateway_association" {
+  count          = length([["10.0.4.0/23"], ["10.0.6.0/23"]])
+  subnet_id      = module.private_subnet.subnet_ids[count.index]
+  nat_gateway_id = module.nat.nat_id
+  depends_on = [ module.private_subnet ]
+}
+
+
 
 # module "nsg" {
 #   source = "./module/network_security_group"
