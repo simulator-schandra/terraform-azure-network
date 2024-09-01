c5dfafa2b32cb03ea9f76f20e4532e4770021f45
Suyash Chandra <suyash.chandra@fci-ccm.com>
2024-09-01 16:36:06 +0530

    suyash : wip

--- main.tf
+++ main.tf
@@ -28,99 +28,21 @@ module "private_subnet" {
   depends_on  = [module.vnet]
 }
 
-# module "nat" {
-#   source            = "./module/nat_gateway"
-#   nat_name          = "simulator-nat"
-#   rg_name           = "simulator-rg"
-#   nat_location      = "South India"
-#   create_pip_prefix = false
-#   tags = {
-#     "Environment" = "Staging"
-#   }
-#   depends_on = [module.private_subnet]
-# }
+module "nat" {
+  source            = "./module/nat_gateway"
+  nat_name          = "simulator-nat"
+  rg_name           = "simulator-rg"
+  nat_location      = "South India"
+  create_pip_prefix = false
+  tags = {
+    "Environment" = "Staging"
+  }
+  depends_on = [module.private_subnet]
+}
 
-# resource "azurerm_subnet_nat_gateway_association" "subnet_nat_gateway_association" {
-#   count          = length([["10.0.4.0/23"], ["10.0.6.0/23"]])
-#   subnet_id      = module.private_subnet.subnet_ids[count.index]
-#   nat_gateway_id = module.nat.nat_id
-#   depends_on     = [module.nat]
-# }
-
-# module "pvt_rt" {
-#   source      = "./module/route_table"
-#   rt_name     = "simulator-pvt-rt"
-#   rt_location = "South India"
-#   rg_name     = "simulator-rg"
-#   rt_route = [
-#     {
-#       name           = "route1"
-#       address_prefix = "10.0.0.0/20"
-#       next_hop_type  = "VnetLocal"
-#     },
-#     {
-#       name           = "route2"
-#       address_prefix = "10.0.0.0/20"
-#       next_hop_type  = "Internet"
-#     }
-#   ]
-#   tags = {
-#     "Environment" = "Staging"
-#   }
-#   depends_on = [module.vnet]
-# }
-
-# resource "azurerm_subnet_route_table_association" "subnet_route_table_association" {
-#   count          = length([["10.0.4.0/23"], ["10.0.6.0/23"]])
-#   subnet_id      = module.private_subnet.subnet_ids[count.index]
-#   route_table_id = module.pvt_rt.rt_id
-#   depends_on     = [module.private_subnet]
-# }
-
-
-
-# module "nsg" {
-#   source = "./module/network_security_group"
-#   nsg_name = "simulator-nsg"
-#   nsg_location = "South India"
-#   rg_name = "simulator-rg"
-#   nsg_rule = [
-#     {
-#       name                       = "ssh_access_rule"
-#       priority                   = 100
-#       direction                  = "Inbound"
-#       access                     = "Allow"
-#       protocol                   = "Tcp"
-#       source_port_range          = "*"
-#       destination_port_range     = "22"
-#       source_address_prefix      = "*"
-#       destination_address_prefix = "*"
-#     }
-#   ]
-#   tags = {
-#     "Environment" = "Staging"
-#   }
-
-# }
-
-# module "vm" {
-#   source = "./module/virtual_machine"
-#   vm_name = "simulator-vm"
-#   vm_location = "South India"
-#   rg_name = "simulator-rg"
-#   subnet_id = "/subscriptions/d13e065d-de62-4283-8742-8aa75745e71d/resourceGroups/simulator-rg/providers/Microsoft.Network/virtualNetworks/simulator-vnet/subnets/simulator-sub-pub-1"
-#   vm_size = "Standard_B1s" 
-#   vm_priority = "Regular"
-#   admin_username = "adminuser"
-#   admin_username_pub_key = "~/.ssh/suyash.pub"
-#   os_disk_caching = "ReadWrite"
-#   os_disk_size_gb = "30"
-#   os_disk_storage_account_type = "Standard_LRS"
-#   source_image_offer = "0001-com-ubuntu-server-jammy"
-#   source_image_publisher = "Canonical"
-#   source_image_sku = "22_04-lts"
-#   source_image_version = "latest"
-#   tags = {
-#     "Environment" = "Staging"
-#   }
-# }
+resource "azurerm_subnet_nat_gateway_association" "subnet_nat_gateway_association" {
+  count          = length([["10.0.4.0/23"], ["10.0.6.0/23"]])
+  subnet_id      = module.private_subnet.subnet_ids[count.index]
+  nat_gateway_id = module.nat.nat_id
+  depends_on     = [module.nat]
+}
